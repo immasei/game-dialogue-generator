33c193990ba8a27ccc8f6420e9c9ce80eda7b3ec
yzho5574 <yzho5574@uni.sydney.edu.au>
2024-09-06 13:36:48 +1000

    springboot application set up

---
+++ src/main/java/com/example/game_dialogue_generator/GameDialogueGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.example.game_dialogue_generator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameDialogueGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameDialogueGeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/game_dialogue_generator/GameDialogueGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.game_dialogue_generator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameDialogueGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
