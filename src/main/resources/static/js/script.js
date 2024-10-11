c162627cea38d4973ab4c9b9d14073377b5b0da9
Xinyi He <xihe0208@uni.sydney.edu.au>
2024-10-11 13:31:23 +1100

    new change of Title of Dialogue page

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -48,6 +48,41 @@ function returnToContent() {
     document.querySelector('.content').style.display = 'block';
     document.querySelector('.content1').style.display = 'none';
 }
+// PDF download
+document.getElementById('download-pdf').addEventListener('click', function() {
+    const { jsPDF } = window.jspdf;
+
+    const doc = new jsPDF();
+
+    // Gets the content to export, assuming the entire page content is in .treeContent
+    let content = document.querySelector('.treeContent').innerText;
+
+    doc.text(content, 10, 10);
+
+    doc.save('page-content.pdf');
+});
+
+// JSON download
+document.getElementById('download-json').addEventListener('click', function() {
+    let content = document.querySelector('.treeContent').innerText;
+    let jsonContent = JSON.stringify({ content: content });
+
+    const a = document.createElement('a');
+    a.href = 'data:text/json;charset=utf-8,' + encodeURIComponent(jsonContent);
+    a.download = 'page-content.json';
+    a.click();
+});
+
+// TXT download
+document.getElementById('download-txt').addEventListener('click', function() {
+    let content = document.querySelector('.treeContent').innerText;
+
+    // creat the txt
+    const a = document.createElement('a');
+    a.href = 'data:text/plain;charset=utf-8,' + encodeURIComponent(content);
+    a.download = 'page-content.txt';
+    a.click();
+});
 
 // Event listeners
 document.querySelector(".tit svg").addEventListener("click", toggleTitDiv);
