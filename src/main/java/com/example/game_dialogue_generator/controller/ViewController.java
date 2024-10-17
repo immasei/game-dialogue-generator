ef05f986e3d49015e4c14976c9d3065c961ba361
megan <mgoc2312@uni.sydney.edu.au>
2024-10-17 22:32:22 +1100

    feat: send prompt to backend to generate dialogue

--- src/main/java/com/example/game_dialogue_generator/controller/ViewController.java
+++ src/main/java/com/example/game_dialogue_generator/controller/ViewController.java
@@ -1,6 +1,8 @@
 package com.example.game_dialogue_generator.controller;
 
+import com.example.game_dialogue_generator.model.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -12,7 +14,12 @@ public class ViewController {
 
     @GetMapping("/home")
     // prompt
-    public String home() {
+//    public String home() {
+//        return "home";
+//    }
+    public String home(ModelMap model) {
+        // TODO: Find username of logged-in password
+        model.addAttribute("user", "defaultusername");
         return "home";
     }
 
