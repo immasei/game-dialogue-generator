e2f43654a097647972d4f32338d575f18956404e
megan <mgoc2312@uni.sydney.edu.au>
2024-10-18 03:26:21 +1100

    send outputmessage to output.html

--- src/main/java/com/example/game_dialogue_generator/controller/ViewController.java
+++ src/main/java/com/example/game_dialogue_generator/controller/ViewController.java
@@ -1,9 +1,13 @@
 package com.example.game_dialogue_generator.controller;
 
+import com.example.game_dialogue_generator.dto.OutputMessageDTO;
 import com.example.game_dialogue_generator.model.User;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class ViewController {
@@ -13,18 +17,27 @@ public class ViewController {
     }
 
     @GetMapping("/home")
-    // prompt
-//    public String home() {
-//        return "home";
-//    }
-    public String home(ModelMap model) {
-        // TODO: Find username of logged-in password
-        model.addAttribute("user", "defaultusername");
+//     prompt
+    public String home() {
         return "home";
     }
 
     @GetMapping("/output")
     public String output() {return "output";}
+
+    // Pass OutputMessage to output by OutputMessage id
+    @GetMapping("/output/{id}")
+    public String outputId(@PathVariable("id") Long id, Model model) {
+        model.addAttribute("id", id);
+        return "output";
+    }
+
+    // Pass OutputMessage to output directly (not working)
+    @PostMapping("/output/message")
+    public String outputMessage(@RequestBody OutputMessageDTO outputMessage, Model model) {
+        model.addAttribute("outputMessage", outputMessage);
+        return "output";
+    }
 
     @GetMapping("/archive")
     public String archive() {
